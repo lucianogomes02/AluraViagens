93083925a3fa97a3af7917a5b3eda26272f5348e
Luciano Gomes <lucianogvda02@gmail.com>
2023-08-09 02:06:04 -0300

    feat(AluraViagens/AluraViagens/View/Viagem/ViagemTableViewCell.swift): implementa ViagemTableViewCell

---
+++ AluraViagens/AluraViagens/View/Viagem/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Luciano Gomes on 09/08/23.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
