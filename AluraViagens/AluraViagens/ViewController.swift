e688a52a0726cf896b42481de7bb95a6af714cf0
Luciano Gomes <lucianogvda02@gmail.com>
2023-08-08 21:03:06 -0300

    feat(AluraViagens/AluraViagens/ViewController.swift): implementa extensão de ViewController com UITableViewDataSource

--- AluraViagens/AluraViagens/ViewController.swift
+++ AluraViagens/AluraViagens/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viagens: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viagens.dataSource = self
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        celula.textLabel?.text = "viagem \(indexPath.row)"
+        
+        return celula
+    }
 }
 
