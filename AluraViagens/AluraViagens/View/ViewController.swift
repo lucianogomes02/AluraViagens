947f5bdbdc961fd3c05160ec44c014add2de8e65
Luciano Gomes <lucianogvda02@gmail.com>
2023-08-09 02:19:22 -0300

    feat(AluraViagens/AluraViagens/View/ViewController.swift): renderiza viagens

--- AluraViagens/AluraViagens/View/ViewController.swift
+++ AluraViagens/AluraViagens/View/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viagens.dataSource = self
-        viagens.delegate = self
+        configuraTableView()
         
         view.backgroundColor = UIColor(red: 30.0/255, green: 59.0/255, blue: 119.0/255, alpha: 1)
+    }
+    
+    func configuraTableView() {
+        viagens.register(UINib(nibName: "ViagemTableViewCell", bundle: nil), forCellReuseIdentifier: "ViagemTableViewCell")
+        viagens.dataSource = self
+        viagens.delegate = self
     }
 
 }
@@ -27,11 +32,9 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        guard let viagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else {fatalError("Erro para criar ViagemTableViewCell")}
         
-        celula.textLabel?.text = "viagem \(indexPath.row)"
-        
-        return celula
+        return viagem
     }
 }
 
@@ -45,6 +48,10 @@ extension ViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 300
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
     }
 }
 
