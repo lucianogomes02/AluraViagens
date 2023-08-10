f890d1dae96f8561f16aef525ec92d9f0cd163e5
Luciano Gomes <lucianogvda02@gmail.com>
2023-08-09 22:18:59 -0300

    feat(AluraViagens/AluraViagens/View/Viagem/ViagemTableViewCell.swift): implementa configurarCelulas para setar valores das viagens

--- AluraViagens/AluraViagens/View/Viagem/ViagemTableViewCell.swift
+++ AluraViagens/AluraViagens/View/Viagem/ViagemTableViewCell.swift
@@ -11,7 +11,7 @@ class ViagemTableViewCell: UITableViewCell {
 
     @IBOutlet weak var backgroundViewCell: UIView!
     
-    @IBOutlet weak var viagemImage: UIView!
+    @IBOutlet weak var viagemImage: UIImageView!
     
     @IBOutlet weak var tituloViagemLabel: UILabel!
     
@@ -24,5 +24,29 @@ class ViagemTableViewCell: UITableViewCell {
     @IBOutlet weak var precoViagemLabel: UILabel!
     
     @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
+    
+    
+    func configuraCelulas(_ viagem: Viagem?) {
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+        
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(
+            string: "R$ \(viagem?.precoSemDesconto ?? 0)"
+        )
+        
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numeroDeDias == 1 ? "Diária" : "Diárias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+        
+    }
     
 }
